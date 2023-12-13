086639418c1dc30883de76814dcd24d32ba52042
Victor Hugo <victorhugo10.2003@hotmail.com>
2023-12-13 13:26:53 -0300

    feats: calculo de multiplicação

--- index.js
+++ index.js
@@ -65,8 +65,55 @@ function gerarPaginaPotencia(requisicao, resposta){
         return;
     }
 }
+
+function gerarPaginaTabuada(requisicao, resposta){
+    try{
+        const numero = Number(requisicao.query.numero);
+        let conteudoResposta = `
+        <!DOCTYPE html>
+        <html lang="pt-br">
+        <head>
+            <meta charset="UTF-8" />
+            <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+            <title>Tabuada do numero ${numero}</title>
+        </head>
+        <body>
+            <h1>Tabuada do numero ${numero}</h1>
+            <ul>
+        `
+        for(let i=0;i<11;i++){
+            const linha = `<li>${numero} multiplicado por ${i} = ${numero*i}</li>`
+            conteudoResposta += linha;
+        }
+        conteudoResposta+=`
+            </ul>
+        </body>
+        </html>
+        `
+        resposta.end(conteudoResposta)
+    } catch(erro){
+        resposta.end(`
+        <!DOCTYPE html>
+        <html lang="pt-br">
+        <head>
+            <meta charset="UTF-8" />
+            <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+            <title>Erro</title>
+        </head>
+        <body>
+            <h1>Não foi possível processar a sua requisição</h1>
+            <h2>Erro ao tentar gerar os resultados</h2>
+            <h2>Na barra de endereço digite http://http://localhost:3000/tabuada?numero=2</h2>
+            <h3>${erro.message}</h3>
+        </body>
+        </html>`)
+        return;
+    }
+}
+
 app.get('/', paginaInicial)
 app.get('/potencia', gerarPaginaPotencia)
+app.get('/tabuada', gerarPaginaTabuada)
 
 app.listen(porta, host, () =>{
     console.log(`Servidor executando em http://${host}:${porta}.`);
